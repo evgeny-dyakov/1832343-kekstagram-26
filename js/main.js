052c609663a3b995203176fc0cc33e527cf7d84e
e.dyakov <e3001dk@gmail.com>
2022-06-18 14:48:38 +0300

    доработка задания больше деталей

--- js/main.js
+++ js/main.js
@@ -1,71 +1,63 @@
-function checkStringLength (string, maxLength) {
-  return string.length <= maxLength;
-}
+const PHOTO_DESCRIPTIONS_COUNT = 25;
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
+const MAX_COMMENTS = 5;
+const USER_NAMES = ['Виктория', 'Александр', 'Света', 'Мария', 'Лиза', 'Анна', 'Роман', 'Максим', 'Сергей'];
+const MESSAGES = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+const DESCRIPTIONS = ['Описание_1', 'Описание_2', 'Описание_3', 'Описание_4', 'Описание_5'];
 
-checkStringLength('тестовая строка', 140);
+const getRandomNamber = (min, max) =>  Math.floor(Math.random() * (max - min + 1)) + min;
 
-function getRandomNamber (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+const getName = () => USER_NAMES[getRandomNamber(0, USER_NAMES.length - 1)];
 
-function getUnorderedNumbers (min, max) {
-  const orderedNumbers = [];
-  const unorderedNumbers = [];
+const getAvatar = () => `img/avatar-${getRandomNamber(1, 6)}.svg`;
 
-  for (let i = min; i <= max; i++) {
-    orderedNumbers.push(i);
-  }
+const getLikes = () => getRandomNamber(MIN_LIKES, MAX_LIKES);
 
-  for (let i = min; i <= max; i++) {
-    const randomNumber = getRandomNamber(0, orderedNumbers.length - 1);
-    unorderedNumbers.push(orderedNumbers.splice(randomNumber, 1)[0]);
-  }
+const getDescription = () => DESCRIPTIONS[getRandomNamber(0, DESCRIPTIONS.length - 1)];
 
-  return unorderedNumbers;
-}
+const getMessage = () => MESSAGES[getRandomNamber(0, MESSAGES.length - 1)];
 
-function getPhotoComments () {
+const commentsId = [];
 
-  let commentId = 0;
-
-  return function (quantity) {
-    const names = ['Виктория', 'Александр', 'Света', 'Мария', 'Лиза', 'Анна', 'Роман', 'Максим', 'Сергей'];
-    const comments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-
-    const photoComments = [];
-    for (let i = 1; i <= quantity; i++) {
-      const comment = {
-        id: ++commentId,
-        name: names[getRandomNamber(0, names.length - 1)],
-        avatar: `img/avatar-${getRandomNamber(1, 6)}.svg`,
-        message: comments[getRandomNamber(0, comments.length - 1)],
-      };
-      photoComments.push(comment);
+const getId = () => {
+  for (;;) {
+    const id = getRandomNamber(1, 999);
+    if (!commentsId.find((el) => el === id)) {
+      commentsId.push(id);
+      return id;
     }
+  }
+};
 
-    return photoComments;
-  };
-}
+const getComments = () => {
+  const count = getRandomNamber(1, MAX_COMMENTS);
+  const comments = [];
+  for (let i = 1; i <= count; i++) {
+    const comment = {
+      id: getId(),
+      name: getName(),
+      avatar: getAvatar(),
+      message: getMessage(),
+    };
+    comments.push(comment);
+  }
+  return comments;
+};
 
-function getPhotoDescriptions (quantity) {
+function getPhotoDescriptions (count) {
   const photoDescriptions = [];
-  const unorderedNumbers = getUnorderedNumbers(1, quantity);
-  const comments = getPhotoComments();
-
-  for (let i = 1; i <= quantity; i++) {
+  for (let i = 1; i <= count; i++) {
     const photoDescription = {
       id: i,
-      url: `photos/${unorderedNumbers[i - 1]}.jpg`,
-      description: 'тестовое описание фотографии',
-      likes: getRandomNamber(15, 200),
-      comments: comments(getRandomNamber(1, 5)),
+      url: `photos/${i}.jpg`,
+      description: getDescription(),
+      likes: getLikes(),
+      comments: getComments(),
     };
     photoDescriptions.push(photoDescription);
   }
-
   return photoDescriptions;
 }
-
-const PHOTO_DESCRIPTIONS_COUNT = 5;
 
 getPhotoDescriptions(PHOTO_DESCRIPTIONS_COUNT);
